2dbfdf10908247a6fbf0b0b636b4465f5603cfdc
Mohammed Ehab <mohammedehab93@gmail.com>
2019-05-17 20:36:07 +0400

    Fixed case mismatch between remote and local

---
+++ src/components/Layout.js
@@ -0,0 +1,52 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+
+import Header from "./Header"
+import Footer from "./Footer"
+
+import "../styles/layout.scss"
+import "bootstrap/dist/css/bootstrap.css"
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div style={{ position: "relative" }}>
+        <Header />
+        <div
+          style={{
+            margin: `0 auto`,
+            padding: `1.5rem 1.0875rem 7rem`,
+            background: "whitesmoke",
+            minHeight: "100vh",
+          }}
+        >
+          <main>{children}</main>
+        </div>
+        <Footer />
+      </div>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
