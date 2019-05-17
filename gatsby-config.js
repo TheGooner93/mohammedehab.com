a163d9e83624197936532af829e5483ab0063896
Mohammed Ehab <mohammedehab93@gmail.com>
2019-05-18 00:21:54 +0400

    Manifest settings for PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,13 +40,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Mohammed Ehab`,
+        short_name: `M.E.`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `black`,
+        theme_color: `black`,
         display: `minimal-ui`,
-        icon: `src/images/LogoTN.png` // This path is relative to the root of the site.
+        icon: `src/images/LogoTN.png`, // This path is relative to the root of the site.
+        include_favicon: true
       }
     },
     {
