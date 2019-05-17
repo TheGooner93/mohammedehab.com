89010799fe99c0cadc1d0e918295a5de38376985
Mohammed Ehab <mohammedehab93@gmail.com>
2019-05-17 22:49:13 +0400

    Removed query keywords from graphql queries

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -1,6 +1,6 @@
-import React from "react"
-import { StaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
@@ -16,17 +16,15 @@ import Img from "gatsby-image"
 const ProfileImage = () => (
   <StaticQuery
     query={graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "dp-badge.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
-            }
+      placeholderImage: file(relativePath: { eq: "dp-badge.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
           }
         }
       }
     `}
     render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
   />
-)
-export default ProfileImage
+);
+export default ProfileImage;
