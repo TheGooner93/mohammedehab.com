788f1d83dffe7dfa5860acfe3ef71b4afe589a9f
Mohammed Ehab <mohammedehab93@gmail.com>
2020-05-20 18:40:51 +0400

    Updated gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { ToastProvider } from 'react-toast-notifications'
 
 import store from './src/state/store';
+import ToastContainer from './src/components/ToastContainer';
 
 export const wrapRootElement = ({ element }) => {
-    console.log("Store: ", store);
-
-    return (
-        <Provider store={store}>{element}</Provider>
-    );
+  return (
+    <Provider store={store}>
+      <ToastProvider components = {{ToastContainer}} placement='bottom-right'>
+        {element}
+      </ToastProvider>
+    </Provider>
+  );
 }
