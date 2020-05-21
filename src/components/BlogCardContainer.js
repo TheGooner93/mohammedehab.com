4151167b8fee04108084aa2c94f9d446a0022f18
Mohammed Ehab <mohammedehab93@gmail.com>
2020-05-21 11:14:11 +0400

    Better load time for blogs

--- src/components/BlogCardContainer.js
+++ src/components/BlogCardContainer.js
@@ -3,6 +3,7 @@ import classNames from 'classnames';
 import { connect } from "react-redux";
 import { Card } from "react-bootstrap";
 import AniLink from "gatsby-plugin-transition-link/AniLink";
+import {randomColor} from 'randomcolor';
 
 const BlogCardContainer = (props) => {
 
@@ -11,19 +12,27 @@ const BlogCardContainer = (props) => {
     const cardView = (
         <div className="blog-card-wrapper">
             <AniLink
-                cover bg={theme === 'night' ? '#2c3e50'  : '' }
+                cover bg={theme === 'night' ? '#2c3e50' : ''}
                 to={post.node.fields.slug}
                 style={{ textDecoration: "none" }}
             >
                 <Card
                     className={classNames('blog-card text-white', theme === 'night' ? 'blog-card_dark' : '')}
                     style={{
-                        backgroundImage: `url(${post.node.frontmatter.thumbnail})`,
-                        backgroundSize: 'cover',
-                        backgroundPosition: "center",
+                        // backgroundImage: `url(${post.node.frontmatter.thumbnail})`,
+                        // backgroundSize: 'cover',
+                        // backgroundPosition: "center",
+                        background: randomColor({
+                            luminosity: theme === 'day' ? 'dark' : 'light',
+                            format: 'rgba',
+                            alpha: 0.5, 
+                        })
                     }}
                 >
-                    <Card.Body className="blog-card_blur">
+                    <Card.Body className={classNames({
+                        'blog-card_text' : theme === 'day',
+                        'blog-card_text_dark' : theme === 'night',
+                    })}>
                         <Card.Title>
                             <h3>{post.node.frontmatter.title}</h3>
                         </Card.Title>
